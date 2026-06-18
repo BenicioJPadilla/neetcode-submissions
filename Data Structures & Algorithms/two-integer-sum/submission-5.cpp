59e6aa8f5101629736c19b5d4af06a040e433ea7
Benicio J. Padilla <beniciopadilla8@gmail.com>
2026-06-17 18:09:42 -0700

    Add: two-integer-sum - submission-5

---
+++ Data Structures & Algorithms/two-integer-sum/submission-5.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        // hashmap that stores distance from target and position
+        unordered_map <int, int> dis_pos;
+
+        // will let us know what position a distance is at
+        // go through each num
+        // check if it is the neccesary distance another pair needs
+        // if it is, it returns that pair
+        // if not returns an empty array
+        for (auto i = 0; i <= nums.size()-1; i++) {
+
+            auto distance =  target - nums[i];
+
+            if (dis_pos.contains(distance)){
+                return {dis_pos[distance], i};
+            }
+            dis_pos[nums[i]] = i;
+        }
+
+        return {};
+    }
+};
